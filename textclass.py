6e7f98b8976ffadcc706fa6f3a67172885d6f6f9
Tanasie <tanasie.vlad@dandsltd.com>
2021-01-25 17:46:48 +0200

    installed and updated libraries for text processing

---
+++ textclass.py
@@ -0,0 +1,13 @@
+#!/usr/bin python3
+
+import matplotlib.pyplot as plt
+import os
+import re
+import shutil
+import string
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+from tensorflow.keras import preprocessing
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
