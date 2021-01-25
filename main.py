0215b85ec8f98cc9c46d2ce29acdaa78428aad09
Tanasie <tanasie.vlad@dandsltd.com>
2021-01-25 13:10:41 +0200

    main can convert any pdf to image and extract_invoice_data from minilib.py can extract invoice number, date and total only with regex for only one type of invoice

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
 #!/usr/bin python3
-import cv2
+
 import minilib as mi
-import pytesseract
-import re
 import os
 
 cwd = os.getcwd()+"/input/"
 output = os.getcwd()+"/output/"
 
-# for file in os.listdir(cwd):
-#     file_path = cwd + file
-#     mi.convert_pdf_to_image(file_path, output, file)
+for file in os.listdir(cwd):
+    file_path = cwd + file
+    mi.convert_pdf_to_image(file_path, output, file)
 
-
-image_data = pytesseract.image_to_string(
-    "./output/101-YYY-LLC.pdf.jpg0001-1.jpg")
-
-print(image_data)
-
-# Extract invoice data from the invoice_data variable using regex
-# - Extract invoice number
-invoice_number = re.search("Invoice # [\d]*", image_data)
-print(invoice_number.group(0))
-
-
-# - Extract invoice date
-# - Extract invoice total
+for file in os.listdir(output):
+    file_path = output + file
+    mi.extract_invoice_data(file_path, "invoice_data.csv")
--- minilib.py
+++ minilib.py
@@ -2,6 +2,9 @@ import cv2
 import pytesseract
 from pdf2image import convert_from_path
 import PyPDF2 as pdf
+import re
+import csv
+import os
 
 
 def draw_box_on_chars(img):
@@ -18,11 +21,11 @@ def draw_box_on_chars(img):
     cv2.waitKey(0)
 
 
-def write_string_to_file(string):
+def write_string_to_file(string, file_name):
     """
     Writes string to file
     """
-    with open("output.txt", "w") as f:
+    with open(file_name, "w") as f:
         f.write(string)
 
 
@@ -51,3 +54,33 @@ def convert_pdf_to_image(pdf_path, output_folder, file_name):
     """
     image = convert_from_path(
         pdf_path, output_folder=output_folder, fmt="jpeg", output_file=f"{file_name}.jpg")
+
+
+def extract_invoice_data(invoice_image_path, csv_file):
+    """
+    Extract inv number, date and total from invoice as image
+    Writes data to csv file provided in format Number, Date, Total
+    """
+    # Extract image data to string
+    image_data = pytesseract.image_to_string(invoice_image_path)
+
+    # - Extract invoice number
+    invoice_number = re.search("Invoice # [\d].*", image_data)
+    number = invoice_number.group(0).split()[2]
+
+    # - Extract invoice date
+    invoice_date = re.search("Invoice Date [\d].*", image_data)
+    date = invoice_date.group(0).split()[2]
+
+    # - Extract invoice total
+    invoice_total = re.search("TOTAL [a-z]* [\d].*", image_data)
+    total = invoice_total.group(0).split()[2]
+
+    # Write image data to csv
+    with open(csv_file, "a", newline='') as file:
+        writer = csv.writer(file)
+
+        if os.path.getsize(csv_file) == 0:
+            writer.writerow(["Number", "Date", "Total"])
+
+        writer.writerow([number, date, total])
